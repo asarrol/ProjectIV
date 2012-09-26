de87b6f3e69bd4bc10145f26382f54c900d706e1
Eric Burns <ericjosephburns@gmail.com>
2012-09-26 01:55:54 -0500

    Deleted unused constants

--- src/com/example/simplestopwatch/Constants.java
+++ src/com/example/simplestopwatch/Constants.java
@@ -6,20 +6,7 @@ package com.example.simplestopwatch;
 public interface Constants {
 
     // misc constants
-  
     int SEC_PER_TICK  = 1;
     int SEC_PER_MIN   = 60;
-    int SEC_PER_HOUR  = 3600;  
-
-    // incoming events
-
-    
-    int STOP = 0;
-    int START = 1;
-    int RESET = 2;
-    int LAP = 3;
-
-    // outgoing events
-
-    String NOW        = "Now";
+    int SEC_PER_HOUR  = 3600;
 }
