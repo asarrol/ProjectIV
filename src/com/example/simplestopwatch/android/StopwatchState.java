65cac3c654328409e74ccb0e9e2c0d54ce8b849c
Konstantin Laufer <laufer@cs.luc.edu>
2013-11-08 11:26:08 -0600

    second step of merge heads

--- src/com/example/simplestopwatch/android/StopwatchState.java
+++
@@ -1,7 +0,0 @@
-package com.example.simplestopwatch.android;
-
-public interface StopwatchState {
-	public void onStartStop(MainActivity adapter);
-	public void onLapReset(MainActivity adapter);
-	public void action(MainActivity adapter);
-}
