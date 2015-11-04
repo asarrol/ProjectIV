b24d1218795ed9831d35b6fbc32506ab44534969
Konstantin Läufer <klaeufer@gmail.com>
2015-11-04 17:59:42 -0600

    removed public modifier from interface methods, fixes #14
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/time/TimeModel.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/time/TimeModel.java
@@ -9,7 +9,7 @@ package edu.luc.etl.cs313.android.simplestopwatch.model.time;
 public interface TimeModel {
     void resetRuntime();
     void incRuntime();
-    public int getRuntime();
-    public void setLaptime();
-    public int getLaptime();
+    int getRuntime();
+    void setLaptime();
+    int getLaptime();
 }
